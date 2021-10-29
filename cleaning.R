078fa70335ac529afb8c4a4b941a6a9a70b809c8
tpark49 <typ5120@gmail.com>
2021-10-28 21:29:44 -0400

    added cleaning

---
+++ cleaning.R
@@ -0,0 +1,86 @@
+library(stringr)
+library(dplyr)
+library(tidyr)
+
+df <- read.csv("example_datafile.csv")
+
+
+
+###CLEANING###
+
+#extract number for GPA
+df$GPA = str_replace(df$GPA, "\nGPA", "")
+mode(df$GPA) = "numeric"
+
+#make new column for GPA (weigted vs unweigthed)
+df$GPA_measure = ifelse(df$GPA>4, "weighted", "unweighted")
+
+#pivot long by colleges
+df %>%
+  mutate(Accepted = str_extract_all(df$accepted_schools, "([A-Za-z\\s&'/.-]+,)|([A-Za-z\\s&'/.-]+)")) %>%
+  tidyr::unnest(Accepted) -> df
+
+df$Accepted = str_replace(df$Accepted, ",", "")
+df$Accepted = trimws(df$Accepted)
+
+#number of essays 
+df$num_of_Essays = str_replace(df$num_of_Essays, "Essays", "")
+mode(df$num_of_Essays) = "numeric"
+
+#number of scores 
+df$num_of_Scores = str_replace(df$num_of_Scores, "Scores", "")
+mode(df$num_of_Scores) = "numeric"
+
+#number of Advice 
+df$num_of_Advice = str_replace(df$num_of_Advice, "Advice", "")
+mode(df$num_of_Advice) = "numeric"
+
+#number of ECs 
+df$num_of_ECs = str_replace(df$num_of_ECs, "ECs", "")
+mode(df$num_of_ECs) = "numeric"
+
+#number of Sports 
+df$num_of_Sports = str_replace(df$num_of_Sports, "Sports", "")
+mode(df$num_of_Sports) = "numeric"
+
+#number of Schools 
+df$num_of_schools = str_replace(df$num_of_schools, "Schools", "")
+mode(df$num_of_schools) = "numeric"
+
+#SAT Score - change type to numeric & convert 2/3 of old SAT score
+df$SAT = str_replace(df$SAT, "SAT", "")
+mode(df$SAT) = "numeric"
+
+df = filter(df, SAT >40)
+df$SAT = ifelse(df$SAT>1600, df$SAT*2/3, df$SAT)
+#df$SAT[df$SAT == 222] = 2222
+
+
+
+View(df)
+
+#ACT Score
+df$ACT = str_replace(df$ACT, "ACT", "")
+mode(df$ACT) = "numeric"
+df$ACT[df$ACT == 0] = NA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
